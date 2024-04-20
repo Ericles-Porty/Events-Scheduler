e471f4d848b93044ac6cc32d07bfd97d951b876f
Ericles <ericlesdsantos@gmail.com>
2024-04-20 02:59:52 -0300

    feat: add di in repository

--- app/Config/definitions.php
+++ app/Config/definitions.php
@@ -4,12 +4,19 @@ namespace App\Config;
 
 use DI;
 use App\Databases\PostgresDatabase;
+use App\Repositories\PostRepository;
+use App\Repositories\PostRepositoryInterface;
 use App\Services\LoggerService;
 use App\Services\LoggerServiceInterface;
 use App\Services\PostService;
 use App\Services\PostServiceInterface;
+use Logger\Elasticsearch\ElasticsearchLogger;
 
 return [
+    ElasticsearchLogger::class => new ElasticsearchLogger(
+        $host = 'elasticsearch', // Container name in docker-compose.yml
+        $port = getenv('ELASTICSEARCH_HTTP_PORT') ?: 9200,
+    ),
     PostgresDatabase::class => new PostgresDatabase(
         $host = 'db', // Container name in docker-compose.yml
         $dbName = getenv('POSTGRES_DB'),
@@ -17,5 +24,6 @@ return [
         $password = getenv('POSTGRES_PASSWORD'),
     ),
     PostServiceInterface::class => DI\autowire(PostService::class),
-    // LoggerServiceInterface::class => \DI\create(LoggerService::class),
+    PostRepositoryInterface::class => DI\autowire(PostRepository::class),
+    LoggerServiceInterface::class => \DI\autowire(LoggerService::class),
 ];
